2371623df5311d3084a248098a4d5fcc1beaea5e
robgriffin247 <robgriffin247@hotmail.com>
2024-12-31 00:42:10 +0100

    switched to new approach

--- app.py
+++ app.py
@@ -1,41 +1,38 @@
+from data.get_assets import get_results, get_standings
 import streamlit as st
+import duckdb 
 import polars as pl
-from sportradar.get_current_elitserien import get_current_elitserien
-from sportradar.get_elitserien_fixtures import get_elitserien_fixtures
-from streamlit_components.tables import standings_table, results_table
-from streamlit_components.filters import matches_results_filter, matches_team_filter, matches_round_filter
 
-if "standings" not in st.session_state:
-    st.session_state["obt_standings"] = get_current_elitserien(st.secrets["sportsradar"]["api_key"])
+results = get_results()
+standings = get_standings(results)
 
-if "matches" not in st.session_state:
-    st.session_state["obt_fixtures"] = get_elitserien_fixtures(st.secrets["sportsradar"]["api_key"])
-    
+st.dataframe(standings)
 
-# Uniform components:
-st.header("BandyScout")
+def choose_team(data, menu):
+    menu.selectbox("Team", key="team", options=results["team"].unique().sort().to_list())
 
-standings_tab, results_tab = st.tabs(["Standings", "Results"])
-#standings_tab, results_tab, fixtures_tab, teams_tab, players_tab = st.tabs(["Standings", "Results", "Fixtures", "Teams", "Players"])
+    with duckdb.connect() as con:
+        df = con.sql(f'''
+                     select * from data where team='{st.session_state["team"]}'
+                     ''').pl()
+    return df
+
+def choose_location(data, menu):
+    menu.selectbox("Home/Away", key="location", options=["All", "Home", "Away"])
+        
+    if st.session_state["location"]=='Home':
+        team_results = data.filter(pl.col("home"))
+    elif st.session_state["location"]=='Away':
+        team_results = data.filter(pl.col("home")==False)
+    else:
+        team_results = data
+
+    return team_results
+
+team_menu, location_menu = st.columns([6,4])
+
+team_results = choose_team(results, team_menu)
+team_results = choose_location(team_results, location_menu)
 
 
-# Tabs:
-with standings_tab:
-    standings_table(st.session_state["obt_standings"])
-
-with results_tab:
-    
-    teams_menu, rounds_menu = st.columns([6,4])
-
-    st.session_state["results"] = matches_results_filter()
-    st.session_state["results"] = matches_team_filter(st.session_state["results"], teams_menu)
-    st.session_state["results"] = matches_round_filter(st.session_state["results"], rounds_menu)
-
-    results_table(st.session_state["results"])
-
-
-# TODO:
-# - add fixtures, copying much of results
-# - add team form tab
-#   - form: color guide, home/away, opponent rank/form
-
+st.dataframe(team_results[["date", "home", "opponent", "scored", "conceded", "result"]])
